af08ee79726de9c2697b3f0c88c160e0f1fe25d6
Shiyu Wang <siyu115@gmail.com>
2023-09-24 16:32:51 +0800

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,7 @@ function App() {
 
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
