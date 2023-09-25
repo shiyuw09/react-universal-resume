cfba1d7eabf49686948b1a9567146745e0dd78bb
Shiyu Wang <siyu115@gmail.com>
2023-09-25 16:23:32 +0800

    opt: layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -35,7 +35,7 @@ function Contact() {
                 className="group flex gap-2"
                 target="_blank"
               >
-                <Github className="w-5 inline-block text-gray-550 print:text-black font-normal group-hover:text-gray-700 transition duration-100 ease-in" />
+                <Github className="w-5 inline-block  print:text-black font-normal group-hover:text-gray-700 transition duration-100 ease-in" />
                 <span className="group-hover:underline">
                   github.com/shiyuw09
                 </span>
@@ -49,7 +49,7 @@ function Contact() {
                 href="mailto:shiyu.wang.dev@gmail.com"
                 className="group flex items-center gap-2"
               >
-                <Mail className="w-5 inline-block text-gray-550 print:text-black font-normal group-hover:text-gray-700 transition duration-100 ease-in" />
+                <Mail className="w-5 inline-block print:text-black font-normal group-hover:text-gray-700 transition duration-100 ease-in" />
                 <span className="group-hover:underline">
                   shiyu.wang.dev@gmail.com
                 </span>
--- src/components/MyH2.tsx
+++ src/components/MyH2.tsx
@@ -1,8 +1,11 @@
 function MyH2(props: { text: string }) {
   return (
-    <h2 className="text-gray-550 uppercase text-sm2 font-bold tracking-widest mb-4">
-      {props.text}
-    </h2>
+    <div>
+      <h2 className="uppercase text-sm2 font-bold tracking-wider text-gray-750">
+        {props.text}
+      </h2>
+      <hr className="mb-4 mt-1 h-[2px] bg-gray-750 border-0" />
+    </div>
   );
 }
 
