580e2c54671be76b7f1a46b20b0fb594b7732410
Shiyu Wang <siyu115@gmail.com>
2023-09-25 15:52:50 +0800

    opt: layout for Contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -31,7 +31,7 @@ function Contact() {
             <li className="mt-1.5 leading-normal text-gray-700 text-md">
               <a
                 href="https://github.com/shiyuw09"
-                className="group flex items-center gap-2"
+                className="group flex gap-2"
                 target="_blank"
               >
                 <span className="inline-block text-gray-550 print:text-black font-normal group-hover:text-gray-700 transition duration-100 ease-in">
@@ -51,7 +51,12 @@ function Contact() {
                     <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
                   </svg>
                 </span>
-                <span className="flex-grow underline">github.com/shiyuw09</span>
+                <span className="group-hover:underline">
+                  github.com/shiyuw09
+                </span>
+                <span className="inline-block text-gray-550 font-normal group-hover:text-gray-700 transition duration-100 ease-in">
+                  ↗
+                </span>
               </a>
             </li>
             <li className="mt-1.5 leading-normal text-gray-700 text-md">
@@ -77,8 +82,11 @@ function Contact() {
                     <path d="M3 7l9 6l9 -6"></path>
                   </svg>
                 </span>
-                <span className="flex-grow underline">
+                <span className="group-hover:underline">
                   shiyu.wang.dev@gmail.com
+                </span>
+                <span className="inline-block text-gray-550 font-normal group-hover:text-gray-700 transition duration-100 ease-in">
+                  ↗
                 </span>
               </a>
             </li>
