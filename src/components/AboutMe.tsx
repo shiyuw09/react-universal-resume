3c0030736104ef51ffedd2387f4d1145f9b2ec9a
Shiyu Wang <siyu115@gmail.com>
2023-09-26 22:30:00 +0800

    opt: text color

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,7 +4,7 @@ function AboutMe() {
   return (
     <div className="mb-8">
       <MyH2 text="About me" />
-      <p>
+      <p className="text-gray-700">
         Front-end developer by day, code superhero by night! With a passion for
         clean, efficient design, I bring ideas to life one pixel at a time.
         Making the web a better place, one div at a time.
