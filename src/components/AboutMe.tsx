a0fa7a09fa7b65b3e2532fdccfce484abf7d3996
Shiyu Wang <siyu115@gmail.com>
2023-09-26 21:46:12 +0800

    feat: add about-me section

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,14 @@
-import MyH2 from "./MyH2";
+import MyH2 from './MyH2';
 
 function AboutMe() {
   return (
-    <div className="mb-16">
+    <div className="mb-8">
       <MyH2 text="About me" />
+      <p>
+        Front-end developer by day, code superhero by night! With a passion for
+        clean, efficient design, I bring ideas to life one pixel at a time.
+        Making the web a better place, one div at a time.
+      </p>
     </div>
   );
 }
